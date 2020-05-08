cbe862a5be8098623aca966c04d7ce4a3e7aefc3
paulotrujillo <paulo_trujillo@hotmail.com>
2020-05-08 18:06:13 -0500

    Add another way to declare a default function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Contact = () => {
+export default function Contact () {
     return (
         <div>
             <h1>Contact Us</h1>
             <p>Contact Us page body content</p>
         </div>
     );
-};
-
-export default Contact;
+}
